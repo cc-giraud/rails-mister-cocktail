9740002fde4d7f2e444d8923c48cdb516c2f2a49
chrystèle claude giraud <giraud@therapose-formations.com>
2020-08-14 15:55:58 +0200

    hfh,cfs,,

--- app/controllers/cocktailsController.rb
+++ app/controllers/cocktailsController.rb
@@ -12,10 +12,17 @@ class CocktailsController < ApplicationController
   end
 
   def create
-    @cocktail = Cocktail.new(params[:cocktail])
+    @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
     redirect_to cocktail_path(@cocktail)
   end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- get "cocktails", to: "cocktails#index"
+  # get "cocktails", to: "cocktails#index"
 
-  get "cocktails/new", to: "cocktails#new"
-  get "cocktails/:id", to: "cocktails#show"
-  post "cocktails", to: "cocktails#create"
+  # get "cocktails/new", to: "cocktails#new"
+  # get "cocktails/:id", to: "cocktails#show"
+  # post "cocktails", to: "cocktails#create"
 
   # NB: The `show` route needs to be *after* `new` route.
 
-  get "cocktails/42/edit", to: "cocktails#edit"
-  patch "cocktails/42", to: "cocktails#update"
+  # get "cocktails/42/edit", to: "cocktails#edit"
+  # patch "cocktails/42", to: "cocktails#update"
 
-  delete "doses/25", to: "cocktails#destroy"
+ # delete "doses/:id", to: "cocktails#destroy"
+
+  resources :cocktails, only: [:index, :new, :show, :create]
 end
